e6566397cf0106c2a02da939e1d8c06104c78e73
solen13 <uusolen13@gmail.com>
2024-06-05 17:01:10 +0300

    ilk commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      colors: {
+        customBlueLight: '#509FCF',
+        customBlueDark: '#060531',
+      },
+    },
+  },
+  plugins: [],
+};
